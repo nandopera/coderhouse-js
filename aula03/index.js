2be732c28384cd60f74860111432f3859c59c2d0
Nando Pera <nandopera@gmail.com>
2024-01-23 20:35:07 -0300

    Exercício 1, em aula

--- aula03/index.js
+++ aula03/index.js
@@ -1,8 +1,13 @@
-var umNumero = 5;
+var num = prompt("Digite de 1 a 5");
 
-console.log(umNumero == 5);
-
-if (umNumero == 5) { //verdadeiro
-    // código a ser executado
-    console.log("Olá mundo!")
+if (num == 1) {
+    alert("Um!")
+} else if (num == 2) {
+    alert("Dois!")
+} else if (num == 3) {
+    alert("Três!")
+} else if (num == 4) {
+    alert("Quatro!")
+} else if (num == 5) {
+    alert("Cinco!")
 }
