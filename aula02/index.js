957ff45b4b15c03ca63a548d09ffa2e1c91419ff
Nando Pera <nandopera@gmail.com>
2024-01-23 20:23:32 -0300

    aula 3, if e else

--- aula02/index.js
+++ aula02/index.js
@@ -3,7 +3,10 @@ let sobrenome = prompt("Qual seu sobrenome?");
 let anoAtual = 2024;
 let idadeFimAno = prompt("Qual sua idade?");
 
-let nomeCompleto = nome + " " + sobrenome;
-let anoNascimento = anoAtual - idadeFimAno;
+let idadeNum = Number(idadeFimAno);
+let nomeCompleto = "Seu nome completo é " + nome + " " + sobrenome + " ";
+let anoNascimento = anoAtual - idadeNum;
 
-alert(`Seu nome completo é ${nomeCompleto} e você nasceu em ${anoNascimento}`);
+alert(nomeCompleto + "e nasceu em " + anoNascimento);
+
+// alert(`Seu nome completo é ${nomeCompleto} e você nasceu em ${anoNascimento}`);
---
+++ aula03/index.js
@@ -0,0 +1,8 @@
+var umNumero = 5;
+
+console.log(umNumero == 5);
+
+if (umNumero == 5) { //verdadeiro
+    // código a ser executado
+    console.log("Olá mundo!")
+}
